5e373107a9e2ee0bb530abc65088a670249fa719
centuryglass <anthony0857@gmail.com>
2019-07-17 13:15:39 -0500

    Updated basic build test

--- Tests/testModules/basicBuild.py
+++ Tests/testModules/basicBuild.py
@@ -5,8 +5,8 @@ Test that the daemon and parent can compile, install, and execute correctly.
 import sys, os
 moduleDir = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, os.path.join(moduleDir, os.pardir))
-from supportModules import make, pathConstants, testActions, testArgs
-from supportModules.testActions import TestResult
+from supportModules import make, pathConstants, testActions, testArgs, \
+                           testResult
 from abc import ABC, abstractmethod
 
 """
@@ -17,21 +17,20 @@ testArgs -- A testArgs.Values argument object.
 def runTest(testArgs):
     print('Running basic build/install/run test:')
     paths       = pathConstants.paths
-    installPath = paths.appSecureExePath
+    daemonPath  = paths.daemonSecureExePath
     parentPath  = paths.parentSecureExePath
     makeArgs    = make.getBuildArgs(testArgs = testArgs)
-    result = testActions.fullTest(makeArgs, installPath, parentPath, \
-                                  outFile = None, \
-                                  debugBuild = testArgs.debugBuild)
-    testActions.checkResult(result, TestResult.success, '1/1', \
-                            '', testFile = None)
+    result = testActions.fullTest(makeArgs, daemonPath, parentPath, \
+                                  outFile = None)
+    testActions.checkResult(result, '1/1', 'Basic build/install/run test', \
+            testFile = None)
 
 # Run this file's tests alone if executing this module as a script:
 if __name__ == '__main__':
     args = testArgs.read()
     if args.printHelp:
         testDefs.printHelp('buildTest.py', \
-                           'Test if KeyDaemon builds and runs with basic ' \
-                           + 'valid arguments.')
+                           "Test if DaemonFramework's test parent and daemon " \
+                           + 'build and run with basic valid arguments.')
     testActions.setup()
     runTest(args)
